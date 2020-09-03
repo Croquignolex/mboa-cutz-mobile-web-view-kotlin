9cd77832b5a0fc57bc4fae68079c0df05fc0a3c1
Alex Stéphane NGOMBOL <gpetitalex10@gmail.com>
2020-09-03 04:33:12 +0100

    Refactoring

--- app/src/main/java/com/mboacutz/shop/SplashScreen.kt
+++ app/src/main/java/com/mboacutz/shop/SplashScreen.kt
@@ -3,18 +3,18 @@ package com.mboacutz.shop
 import android.os.Bundle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_splash_screen.logo
+import kotlinx.android.synthetic.main.activity_splash_screen.*
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        logo.alpha = 1f
-        val mainActivityIntent = Intent(this, MainActivity::class.java);
+        logo.alpha = 0f
         // Wait 1500 ms
         logo.animate().setDuration(1500).alpha(1f).withEndAction {
             // Start app & close splash screen with transition
+            val mainActivityIntent = Intent(baseContext, MainActivity::class.java);
             startActivity(mainActivityIntent)
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
             finish()
